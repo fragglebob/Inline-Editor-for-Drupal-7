eb74a920653724197f9cbe57c0e1abc60308d003
Robert Wells <robert@fragglebob.me>
2011-09-11 16:48:09 +0100

    Now uses Aloha's events as triggers and JS is a lots more reusable.
    Also now including Aloha as a library.

--- aloha.js
+++ aloha.js
@@ -1,16 +1,38 @@
-function aloha_display_saved(msg){
-  var obj = jQuery.parseJSON(msg);
-  if(obj.status == 'saved'){
-    $('#aloha-' + obj.nid + '-saved').show().delay(1300).fadeOut();
+$(document).ready(function(){
+  GENTICS.Aloha.EventRegistry.subscribe(GENTICS.Aloha,"editableActivated",function(event,eventinfo){
+    aloha_focus_box(eventinfo.editable.getId());
+  });
+  GENTICS.Aloha.EventRegistry.subscribe(GENTICS.Aloha,"editableDeactivated",function(event,eventinfo){
+  
+    var id = eventinfo.editable.getId();
+    var nid = id.replace("aloha-","");
+    var html = $(id).html();
+    
+    aloha_unfocus_box(id);
+    
+    $.ajax({
+      type: "POST",
+      url: Drupal.settings.basePath + "aloha/save/node/" + nid,
+      data: ({ body: eventinfo.editable.getContents(), }),
+      success: function(msg){
+        aloha_display_saved(msg);
+      },
+    });
+  })
+  function aloha_display_saved(msg){
+    var obj = jQuery.parseJSON(msg);
+    if(obj.status == 'saved'){
+      $('#aloha-' + obj.nid + '-saved').show().delay(1300).fadeOut();
+    }
   }
-}
-function aloha_focus_box(element){
-  $(element).css({
-    background:"rgb(255,255,235)"
-  });
-}
-function aloha_unfocus_box(element){
-  $(element).css({
-    background:"none"
-  });
-}
+  function aloha_focus_box(element){
+    $(element).css({
+      background:"rgb(255,255,235)"
+    });
+  }
+  function aloha_unfocus_box(element){
+    $(element).css({
+      background:"none"
+    });
+  }
+});
